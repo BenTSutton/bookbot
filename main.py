fd9378546bf25f00b990b6f8199ea24713c25a0b
BenTSutton <BenThomasSuttonCode@gmail.com>
2023-12-10 15:59:49 +0000

    Done work up to ch 16 Print a report, I hate sorting in python

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def main():
     path_to_file = "books/frankenstein.txt"
-    file_contents = get_book(path_to_file)
-    print(file_contents)
-    word_count(file_contents)
+    print_report(path_to_file)
+    
 
 def get_book(path):
     with open(path) as f:
@@ -10,6 +9,43 @@ def get_book(path):
     
 def word_count(book):
     words = book.split()
-    print(len(words))
+    return(len(words))
+
+def letter_count(book):
+    letters = {}
+    for letter in book:
+        lower_case_letter = letter.lower()
+        if lower_case_letter in letters:
+            letters[lower_case_letter] += 1
+        elif letter.isalpha():
+            letters[lower_case_letter] = 1
+    return(letters)
+
+def sort_letters_dict_to_list(dict):
+    sorted_list = []
+    for letter in dict:
+        sorted_list.append({"letter": letter, "number_of_occurances": dict[letter]})
+
+    sorted_list.sort(reverse=True, key=sort_key)
+    return sorted_list
+
+def sort_key(e):
+    return e["number_of_occurances"]
+
+
+def print_report(path):
+    print(f"--- Begin report of {path} ---")
+    book = get_book(path)
+    book_word_count = word_count(book)
+    print(f"{book_word_count} words found in the document\n\n")
+    letter_dict = letter_count(book)
+
+    sorted_letters = sort_letters_dict_to_list(letter_dict)
+    print(sorted_letters)
+
+    for letter in sorted_letters:
+        print(f"The '{letter['letter']}' character was found {letter['number_of_occurances']}")
+    
+
 
 main()
